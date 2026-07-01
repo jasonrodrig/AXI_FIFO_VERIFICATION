ce0c071a219be244ebf85c1885e0a1282fb8e730
Nishanth Gowda CJ <nishanthgowda@feserver.mirafra.com>
2026-07-01 12:35:05 +0530

    updated interface

--- src/dv/axi4_interface.sv
+++ src/dv/axi4_interface.sv
@@ -4,57 +4,57 @@ interface axi4_if(input logic aclk,
                   input logic aresetn);
 
   // Write Address Channel
-  logic [`AXI_ID_WIDTH-1:0]      awid;
-  logic [`AXI_ADDR_WIDTH-1:0]    awaddr;
-  logic [`AXI_LEN_WIDTH-1:0]     awlen;
-  logic [`AXI_SIZE_WIDTH-1:0]    awsize;
-  logic [`AXI_BURST_WIDTH-1:0]   awburst;
-  logic [`AXI_LOCK_WIDTH-1:0]    awlock;
-  logic [`AXI_CACHE_WIDTH-1:0]   awcache;
-  logic [`AXI_PROT_WIDTH-1:0]    awprot;
-  logic [`AXI_QOS_WIDTH-1:0]     awqos;
-  logic [`AXI_REGION_WIDTH-1:0]  awregion;
-  logic [`AXI_USER_WIDTH-1:0]    awuser;
-  logic                          awvalid;
-  logic                          awready;
+  logic [`id_wid-1:0]      awid;
+  logic [`adr_wid-1:0]     awaddr;
+  logic [`len_wid-1:0]     awlen;
+  logic [`siz_wid-1:0]     awsize;
+  logic [`bst_wid-1:0]     awburst;
+  logic [`loc_wid-1:0]     awlock;
+  logic [`cach_wid-1:0]    awcache;
+  logic [`prot_wid-1:0]    awprot;
+  logic [`qos_wid-1:0]     awqos;
+  logic [`region_wid-1:0]  awregion;
+  logic [`user_wid-1:0]    awuser;
+  logic                    awvalid;
+  logic                    awready;
 
   // Write Data Channel
-  logic [`AXI_DATA_WIDTH-1:0]    wdata;
-  logic [`AXI_STRB_WIDTH-1:0]    wstrb;
-  logic                          wlast;
-  logic [`AXI_USER_WIDTH-1:0]    wuser;
-  logic                          wvalid;
-  logic                          wready;
+  logic [`data_wid-1:0]    wdata;
+  logic [`strb_wid-1:0]    wstrb;
+  logic                    wlast;
+  logic [`user_wid-1:0]    wuser;
+  logic                    wvalid;
+  logic                    wready;
 
   // Write Response Channel
-  logic [`AXI_ID_WIDTH-1:0]      bid;
-  logic [`AXI_RESP_WIDTH-1:0]    bresp;
-  logic [`AXI_USER_WIDTH-1:0]    buser;
-  logic                          bvalid;
-  logic                          bready;
+  logic [`id_wid-1:0]      bid;
+  logic [`rsp_wid-1:0]     bresp;
+  logic [`user_wid-1:0]    buser;
+  logic                    bvalid;
+  logic                    bready;
 
   // Read Address Channel
-  logic [`AXI_ID_WIDTH-1:0]      arid;
-  logic [`AXI_ADDR_WIDTH-1:0]    araddr;
-  logic [`AXI_LEN_WIDTH-1:0]     arlen;
-  logic [`AXI_SIZE_WIDTH-1:0]    arsize;
-  logic [`AXI_BURST_WIDTH-1:0]   arburst;
-  logic [`AXI_LOCK_WIDTH-1:0]    arlock;
-  logic [`AXI_CACHE_WIDTH-1:0]   arcache;
-  logic [`AXI_PROT_WIDTH-1:0]    arprot;
-  logic [`AXI_QOS_WIDTH-1:0]     arqos;
-  logic [`AXI_REGION_WIDTH-1:0]  arregion;
-  logic [`AXI_USER_WIDTH-1:0]    aruser;
-  logic                          arvalid;
-  logic                          arready;
+  logic [`id_wid-1:0]      arid;
+  logic [`adr_wid-1:0]     araddr;
+  logic [`len_wid-1:0]     arlen;
+  logic [`siz_wid-1:0]     arsize;
+  logic [`bst_wid-1:0]     arburst;
+  logic [`loc_wid-1:0]     arlock;
+  logic [`cach_wid-1:0]    arcache;
+  logic [`prot_wid-1:0]    arprot;
+  logic [`qos_wid-1:0]     arqos;
+  logic [`region_wid-1:0]  arregion;
+  logic [`user_wid-1:0]    aruser;
+  logic                    arvalid;
+  logic                    arready;
 
   // Read Data Channel
-  logic [`AXI_ID_WIDTH-1:0]      rid;
-  logic [`AXI_DATA_WIDTH-1:0]    rdata;
-  logic [`AXI_RESP_WIDTH-1:0]    rresp;
-  logic                          rlast;
-  logic [`AXI_USER_WIDTH-1:0]    ruser;
-  logic                          rvalid;
-  logic                          rready;
+  logic [`id_wid-1:0]      rid;
+  logic [`data_wid-1:0]    rdata;
+  logic [`rsp_wid-1:0]     rresp;
+  logic                    rlast;
+  logic [`user_wid-1:0]    ruser;
+  logic                    rvalid;
+  logic                    rready;
 
 endinterface
--- src/dv/define.sv
+++ src/dv/define.sv
@@ -1,22 +1,20 @@
 `ifndef AXI_DEFINES_SV
 `define AXI_DEFINES_SV
 
-// AXI Bus Widths
-`define AXI_ID_WIDTH      4
-`define AXI_ADDR_WIDTH    32
-`define AXI_DATA_WIDTH    64
-
-// AXI Control Widths
-`define AXI_LEN_WIDTH     4
-`define AXI_SIZE_WIDTH    3
-`define AXI_BURST_WIDTH   2
-`define AXI_LOCK_WIDTH    2
-`define AXI_CACHE_WIDTH   2
-`define AXI_PROT_WIDTH    3
-`define AXI_QOS_WIDTH     4
-`define AXI_REGION_WIDTH  4
-`define AXI_USER_WIDTH    4
-`define AXI_RESP_WIDTH    2
-`define AXI_STRB_WIDTH    (`AXI_DATA_WIDTH/8)
+// Width Parameters
+`define id_wid       4
+`define adr_wid      32
+`define data_wid     32
+`define strb_wid     (`data_wid/8)
+`define len_wid      4
+`define siz_wid      3
+`define bst_wid      2
+`define loc_wid      2
+`define cach_wid     4
+`define prot_wid     3
+`define rsp_wid      2
+`define qos_wid      4
+`define region_wid   4
+`define user_wid     4
 
 `endif
